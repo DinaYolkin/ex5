0eceb971313af4525437c00659610cb7d69097e3
leakhodo <lea.khodorkovsky@mail.huji.ac.il>
2018-06-07 19:22:52 +0300

    doco to CommandReader and paramNum to package private

--- src/filesprocessing/CommandReader.java
+++ src/filesprocessing/CommandReader.java
@@ -1,37 +1,55 @@
 package filesprocessing;
+/*
+This file have a class that create an instant of CommandReader.
+ */
 
 import filters.*;
 import orders.*;
 
-
 import java.io.*;
 import java.util.ArrayList;
 
+public class CommandReader {
 
- class CommandReader {
-
+	/* Constants represent messages of type two errors when there is no FILTER row or there is no ORDER row
+	 in the given command file
+	  */
 	private static final String ERROR_NO_FILTER_LINE = "ERROR: no FILTER line";
 	private static final String ERROR_NO_ORDER_LINE = "ERROR: no ORDER line";
-	private static final String NEXT_SECTION = "FILTER";
+
+	/* Constants representing the default objects of Order and Filter as describe in the exercise*/
 	private static final String DEFAULT_ORDER = "abs";
 	private static final String DEFAULT_FILTER = "all";
+
+	/* Constant represents that we are in the first line of the next section*/
+	private static final String NEXT_SECTION = "FILTER";
+
+	/* Constants representing the header lines of Filter and Order*/
 	private static final String FILTER_INDICATOR = "FILTER";
 	private static final String ORDER_INDICATOR = "ORDER";
-	private static final String ERROR_IOEXCEPTION = "ERROR: IOException";
 
-
-
+	/* constant representing the address of the file that the CommandReader can process and read*/
 	private String fileAddress;
+
+	/* Constant representing the factory that produces Filters*/
 	private FilterFactory filterFactory;
+
+	/* Constant representing the factory that produces Orders*/
 	private OrderFactory orderFactory;
 
+	/* The class constructor */
 	CommandReader(String addressFile) {
-
 		fileAddress = addressFile;
 		filterFactory = new FilterFactory();
 		orderFactory = new OrderFactory();
 	}
 
+	/*
+	The method reads the command file and creates an ArrayList of Sections.
+	While reading the command file, create for each Section a Filter, Order and ArrayList of integers
+	 represent the number of the rows where is a type one error, according to the description in the file.
+ 	returns the ArrayList of Sections.
+	 */
 	ArrayList<Section> readFile() throws SectionException, FilterExceptions, OrderExceptions {
 
 		ArrayList<Section> sectionList = new ArrayList<>();
@@ -40,12 +58,13 @@ import java.util.ArrayList;
 
 			BufferedReader readFile = new BufferedReader(new FileReader(this.fileAddress));
 			int numLine = 1;
+			// boolean variable that indicate if there was a order sub-section line at the last section
 			boolean isOrderLine = true;
 
 			String line = readFile.readLine();
 
 			while (line != null) {
-
+				Section section;
 				ArrayList<Integer> warningsLines = new ArrayList<>();
 
 				// filter handling
@@ -67,6 +86,7 @@ import java.util.ArrayList;
 					warningsLines.add(numLine);
 					filter = filterFactory.getFilter(DEFAULT_FILTER);
 				}
+
 				line = readFile.readLine();
 				numLine++;
 
@@ -95,7 +115,7 @@ import java.util.ArrayList;
 					}
 				}
 
-				Section section = new Section(filter, order, warningsLines);
+				section = new Section(filter, order, warningsLines);
 				if (isOrderLine) {
 					line = readFile.readLine();
 					numLine++;
@@ -104,7 +124,7 @@ import java.util.ArrayList;
 
 			}
 		} catch (IOException e) {
-			throw new SectionException(ERROR_IOEXCEPTION);
+			throw new SectionException("ERROR: IOException");
 		}
 
 		return sectionList;
--- src/filesprocessing/InvalidParamsNum.java
+++ src/filesprocessing/InvalidParamsNum.java
@@ -2,12 +2,11 @@ package filesprocessing;
 
 import java.io.IOException;
 
-public class InvalidParamsNum extends IOException {
+ class InvalidParamsNum extends IOException {
 
 	private static final String MSG_ERROR_NUMBER_ARGS = "ERROR: Invalid Number of arguments. Should be 2";
 
-	InvalidParamsNum() {
-		super(MSG_ERROR_NUMBER_ARGS);
+	InvalidParamsNum() {super(MSG_ERROR_NUMBER_ARGS);
 	}
 
 }
